c497b7cc3f63f7c18ea499aab0f3a7ed7da4c58b
Hetty Symes <59455170+hettysymes@users.noreply.github.com>
2022-07-17 10:52:33 +0100

    microbit records index and time

--- HelloWorld/source/main.cpp
+++ HelloWorld/source/main.cpp
@@ -3,7 +3,9 @@
 #define I2C_ADDR 0x3A
 #define ACC_REG 0x01
 #define ACC_BYTE_SIZE 4
+#define DATA_FREQ 30
 
+/*
 //get x and y accelerations
 MicroBit uBit;
 int main() {
@@ -19,7 +21,23 @@ int main() {
     xAcc = buf2[0] << 8 | buf2[1];
     yAcc = buf2[2] << 8 | buf2[3];
     uBit.serial.printf("%d,%d\r\n", xAcc, yAcc);
-    uBit.sleep(100);
+    uBit.sleep(DATA_FREQ);
+  }
+}
+*/
+
+//get coordinates of accelerometer using uBit.accelerometer
+MicroBit uBit;
+int main() {
+  uBit.init();
+  unsigned long timeStart = uBit.systemTime();
+  int xAcc, yAcc;
+  for (int i = 0;;i++) {
+    xAcc = uBit.accelerometer.getX();
+    yAcc = uBit.accelerometer.getY();
+    unsigned long timeNow = uBit.systemTime();
+    uBit.serial.printf("%d,%d,%d,%d\r\n", i, xAcc, yAcc, timeNow - timeStart);
+    uBit.sleep(DATA_FREQ);
   }
 }
 
@@ -33,20 +51,5 @@ int main() {
   uBit.i2c.read(0x3A, buf, 1);
   uBit.serial.printf("Id: %X\r\n", (int)buf[0]);
   release_fiber();
-}
-*/
-
-/*
-//get coordinates of accelerometer using uBit.accelerometer
-MicroBit uBit;
-int main() {
-  uBit.init();
-  for (;;) {
-    int xAcc, yAcc;
-    xAcc = uBit.accelerometer.getX();
-    yAcc = uBit.accelerometer.getY();
-    uBit.serial.printf("%d,%d\r\n", xAcc, yAcc);
-    uBit.sleep(100);
-  }
 }
 */

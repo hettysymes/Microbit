f7b65c735ab1d32d28a8c1baecc4ed5d87731f39
Hetty Symes <59455170+hettysymes@users.noreply.github.com>
2022-07-17 11:38:16 +0100

    added timestamp

--- HelloWorld/source/main.cpp
+++ HelloWorld/source/main.cpp
@@ -3,29 +3,30 @@
 #define I2C_ADDR 0x3A
 #define ACC_REG 0x01
 #define ACC_BYTE_SIZE 4
-#define DATA_FREQ 30
+#define DATA_FREQ 10
 
-/*
 //get x and y accelerations
 MicroBit uBit;
 int main() {
   uBit.init();
+  unsigned long timeStart = uBit.systemTime();
   char buf[] = {ACC_REG};
   char buf2[ACC_BYTE_SIZE+1];
   int16_t xAcc;
   int16_t yAcc;
 
-  for (;;) {
+  for (int i=0;;i++) {
     uBit.i2c.write(I2C_ADDR, buf, 1, true);
     uBit.i2c.read(I2C_ADDR, buf2, ACC_BYTE_SIZE);
     xAcc = buf2[0] << 8 | buf2[1];
     yAcc = buf2[2] << 8 | buf2[3];
-    uBit.serial.printf("%d,%d\r\n", xAcc, yAcc);
+    unsigned long timeNow = uBit.systemTime();
+    uBit.serial.printf("%d,%d,%d,%d\r\n", i, xAcc, yAcc, timeNow - timeStart);
     uBit.sleep(DATA_FREQ);
   }
 }
-*/
 
+/*
 //get coordinates of accelerometer using uBit.accelerometer
 MicroBit uBit;
 int main() {
@@ -40,6 +41,7 @@ int main() {
     uBit.sleep(DATA_FREQ);
   }
 }
+*/
 
 /*
 //get id of accelerometer

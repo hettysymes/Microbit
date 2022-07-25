2fb009ae216bb2aaf4e80770d7b06c138941ac78
Hetty Symes <59455170+hettysymes@users.noreply.github.com>
2022-07-25 07:24:05 +0100

    added button presses

--- HelloWorld/source/main.cpp
+++ HelloWorld/source/main.cpp
@@ -5,10 +5,13 @@
 #define ACC_BYTE_SIZE 4
 #define DATA_FREQ 10
 
-//get x and y accelerations
+/*
+//get x and y accelerations - DIRECT (V1)
 MicroBit uBit;
 int main() {
   uBit.init();
+  uBit.display.setDisplayMode(DISPLAY_MODE_GREYSCALE);
+  uBit.display.image.setPixelValue(0, 0, 10);
   unsigned long timeStart = uBit.systemTime();
   char buf[] = {ACC_REG};
   char buf2[ACC_BYTE_SIZE+1];
@@ -21,27 +24,45 @@ int main() {
     xAcc = buf2[0] << 8 | buf2[1];
     yAcc = buf2[2] << 8 | buf2[3];
     unsigned long timeNow = uBit.systemTime();
-    uBit.serial.printf("%d,%d,%d,%ld\r\n", i, xAcc, yAcc, timeNow - timeStart);
-    uBit.sleep(DATA_FREQ);
-  }
-}
-
-/*
-//get coordinates of accelerometer using uBit.accelerometer
-MicroBit uBit;
-int main() {
-  uBit.init();
-  unsigned long timeStart = uBit.systemTime();
-  int xAcc, yAcc;
-  for (int i = 0;;i++) {
-    xAcc = uBit.accelerometer.getX();
-    yAcc = uBit.accelerometer.getY();
-    unsigned long timeNow = uBit.systemTime();
-    uBit.serial.printf("%d,%d,%d,%d\r\n", i, xAcc, yAcc, timeNow - timeStart);
+    uBit.serial.printf("%d,%d,%d,%ld\n", i, xAcc, yAcc, timeNow - timeStart);
     uBit.sleep(DATA_FREQ);
   }
 }
 */
+
+MicroBit uBit;
+
+void lightStopMode(void) {
+  uBit.display.image.setPixelValue(0, 0, 10);
+  uBit.display.image.setPixelValue(1, 0, 0);
+}
+
+void lightDrawMode(void) {
+  uBit.display.image.setPixelValue(0, 0, 0);
+  uBit.display.image.setPixelValue(1, 0, 10);
+}
+
+int main() {
+  uBit.init();
+  uBit.display.setDisplayMode(DISPLAY_MODE_GREYSCALE);
+  lightStopMode();
+  unsigned long timeStart;
+  int16_t xAcc;
+  int16_t yAcc;
+  while (!uBit.buttonB.isPressed()) {
+    uBit.sleep(DATA_FREQ);
+  }
+  uBit.sleep(1500);
+  lightDrawMode();
+  timeStart = uBit.systemTime();
+  for (int i=0; !uBit.buttonB.isPressed(); i++) {
+    xAcc = uBit.accelerometer.getX();
+    yAcc = uBit.accelerometer.getY();
+    unsigned long timeNow = uBit.systemTime();
+    uBit.serial.printf("%d,%d,%d,%ld\n", i, xAcc, yAcc, timeNow - timeStart);
+    uBit.sleep(DATA_FREQ);
+  }
+}
 
 /*
 //get id of accelerometer

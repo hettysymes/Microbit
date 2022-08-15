d0cad6f34f77288a1eb3db7e13499299f3f1595e
Hetty Symes <59455170+hettysymes@users.noreply.github.com>
2022-08-15 09:30:39 +0100

    new draw prog

--- microbitDraw.py
+++ microbitDraw.py
@@ -3,27 +3,20 @@ import serial
 import time
 import matplotlib.pyplot as plt
 import re
-import sys
 
 COM = 'COM5'
 BAUD_RATE = 115200
 TIMEOUT = 1
-SERIAL_FILE = "serialIn.txt"
 GRAPH_PAUSE = 0.001
-DATA_FREQ = 0.005
+DATA_FREQ = 0.01
+MAX_ITERATIONS = 1000
 
-class SerialIn:
+class Serial:
     def __init__(self):
         self.ser = serial.Serial(COM, BAUD_RATE, timeout=TIMEOUT)
-        open(SERIAL_FILE, 'w').close()
-        time.sleep(GRAPH_PAUSE)
-
-    def writeNext(self):
-        line = self.ser.readline()
-        if line:
-            f = open(SERIAL_FILE, "a")
-            f.write(line.decode())
-            f.close()
+    
+    def readline(self):
+        return self.ser.readline().decode("utf-8")
     
     def close(self):
         self.ser.close()
@@ -62,100 +55,20 @@ class Gui:
         self.yAccGraph.replot()
         plt.pause(GRAPH_PAUSE)
 
-def runSerial():
-    si = SerialIn()
-    while True:
-        si.writeNext()
-        time.sleep(GRAPH_PAUSE)
-
-def runDraw():
-    gui = Gui()
-    f = open(SERIAL_FILE, "r")
-    f.seek(0,2) # Go to the end of the file
-    while True:
-        line = f.readline()
-        if not line:
-            time.sleep(GRAPH_PAUSE) # Sleep briefly
-            continue
-        ret = re.split(",|\n", line)
-        _, xAcc, yAcc, t =  [int(ret[0]), int(ret[1]), int(ret[2]), int(ret[3])]
-        gui.addPoints((t, xAcc), (t, yAcc))
-        time.sleep(GRAPH_PAUSE)
-
-
-if __name__ == '__main__':
-    mode = sys.argv[1]
-    print(mode)
-    if mode == "si":
-        runSerial()
-    else:
-        runDraw()
-# si = SerialIn()
-# gui = Gui()
-# while True:
-#     data = si.getNext()
-#     ret = re.split(",|\r\n", line.decode())
-#         return [int(ret[0]), int(ret[1]), int(ret[2]), int(ret[3])]
-#     _, xAcc, yAcc, t = data
-#     gui.addPoints((t, xAcc), (t, yAcc))
-#     time.sleep(DATA_FREQ)
-
-"""
-class Gui:
-    def __init__(self):
-        self.xs = [0]
-        self.ys = [0]
-        plt.xlabel('x-axis')
-        plt.ylabel('y-axis')
-        plt.title('Microbit drawing') 
-        self.refreshDrawing()
-    
-    def addPoint(self, x, y):
-        self.xs.append(x)
-        self.ys.append(y)
-        self.refreshDrawing()
-
-    def refreshDrawing(self):
-        plt.plot(self.xs, self.ys)
-        plt.pause(GRAPH_PAUSE)
-"""
-
-"""
-coord = [0, 0]
-s = [0, 0]
-u = [0, 0]
-v = [0, 0]
-a = [0, 0]
-tPrev = -1
-si = SerialIn()
+print("Prog started")
+s = Serial()
 gui = Gui()
-while True:
-    [i, a[0], a[1], tNow] = si.getNext()
-    print(i, a)
-    if (tPrev == -1):
-        tPrev = tNow
-        time.sleep(DATA_FREQ)
+for i in range(MAX_ITERATIONS):
+    try:
+        line = s.readline()
+    except:
         continue
-    print(i, a)
-    t = tNow-tPrev
-    tPrev = tNow
-    v[0] = u[0] + a[0]*t
-    v[1] = u[1] + a[1]*t
-    s[0] = ((v[0] + u[0])/2) * t
-    s[1] = ((v[1] + u[1])/2) * t
-    u = list(v)
-    coord[0] += s[0]
-    coord[1] += s[1]
-    gui.addPoint(coord[0], coord[1])
-    time.sleep(DATA_FREQ)
-"""
-
-"""
-si = SerialIn()
-gui = Gui()
-for i in range(100):
-    data = si.getNext()
-    print(f"{data[0]}, {data[1]}")
-    gui.addPoint(data[0], data[1])
-    time.sleep(0.2)
-"""
+    if not line:
+        time.sleep(GRAPH_PAUSE) # Sleep briefly
+        continue
+    ret = re.split(",|\n", line)
+    print(ret)
+    _, xAcc, yAcc, t =  [int(ret[0]), int(ret[1]), int(ret[2]), int(ret[3])]
+    gui.addPoints((t, xAcc), (t, yAcc))
+    time.sleep(GRAPH_PAUSE)
+s.close()
